f6cbff6155cd0d738ad4c749dfc5608e7c0fe868
sanemiliano <zeus@polispay.org>
2019-06-02 21:55:00 -0500

    =providers ready

---
+++ src/pages/add-preferred-user/add-preferred-user.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AddPreferredUserPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-add-preferred-user',
+  templateUrl: 'add-preferred-user.html',
+})
+export class AddPreferredUserPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddPreferredUserPage');
+  }
+
+}
